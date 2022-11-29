6255254247525521cfd9fb0bc0701d9484d1202d
AgnesaS <agnesaselimi8@gmail.com>
2022-11-29 21:30:14 +0100

    Initial Commit

---
+++ Quotes List App/Quotes_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_List_AppApp.swift
+//  Quotes List App
+//
+//  Created by Agnesa  on 29.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
